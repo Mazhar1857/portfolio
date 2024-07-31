8bd68093b7a5889228c0eed8d6149593e01ae26e
Mazhar1857 <ali.mazhar1857@gmail.com>
2024-07-31 11:15:22 +0530

    modify image path

--- src/projectData.js
+++ src/projectData.js
@@ -1,19 +1,16 @@
 export const projects = [
     {
         name: 'News App',
-        img: '/newsApp.png',
+        img: './newsApp.png',
         demoUrl: 'https://mazhar1857.github.io/news-shala/#/top',
         gitUrl: 'https://github.com/Mazhar1857/news-shala',
         type: 'advance'
     },
     {
         name: 'Country Info',
-        img: '/countryInfo.png',
+        img: './countryInfo.png',
         demoUrl: 'https://mazhar1857.github.io/country-info/',
         gitUrl: 'https://github.com/Mazhar1857/country-info',
         type: 'advance'
     }
-
-
-
 ]
