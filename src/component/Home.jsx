4d82e764e46bf23f0bf5f99e80f859ba6b6707dd
Mazhar1857 <ali.mazhar1857@gmail.com>
2024-08-09 17:25:23 +0530

    fixed scroll to section bug

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import './Home.css'
 import pic from '../assets/pic-1.png'
+import { Link } from 'react-router-dom'
 const Home = () => {
+
+    const scrollToSection = (id) => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' })
+        }
+    }
+
     return (
         <div className='home' >
             <div>
@@ -15,9 +24,9 @@ const Home = () => {
                     <a href="./Mazhar_frontend.pdf" download='Mazhar_frontend.pdf'>
                         <button>Download CV</button>
                     </a>
-                    <a href="#contact">
+                    <Link to='/' onClick={() => scrollToSection('contact')}>
                         <button>Contact Info</button>
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
